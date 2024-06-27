2c021517daaacf38a3fd474e3dfd4b88fd8c5e91
Sunny <sunnysagar620@gmail.com>
2024-06-28 01:35:09 +0530

    commit the latest

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import './home.css'
+import video from '../../Assests/video.mp4'
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+
+
+
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src= {video} muted autoPlay loop type= "video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+          
+          <span className="smallText">
+            Our Packages
+          </span>
+          
+          <h1 className="homeTitle">
+            Search your Holiday
+          </h1>
+
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">
+              Search your destination:
+            </label>
+            <div className="input flex">
+              <input type="text" placeholder='Enter name here....'/>
+              <GrLocation className="icon"/>
+
+            </div>
+          </div>
+
+          <div className="dateInput">
+            <label htmlFor="city">
+              Search your date:
+            </label>
+            <div className="input flex">
+              <input type="date"/>
+
+            </div>
+          </div>
+
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">
+                Max price:
+              </label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max= "5000" min="1000" />
+            </div>
+
+          </div>
+
+
+           <div className="searchOPtions flex">
+              <HiFilter className="icon"/>
+              <span>MORE FILTERS</span>
+           </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import {TbGridDots} from 'react-icons/tb'
 
 const Navbar = () => {
+  const [active, setActive] = useState('navBar')
+  // Function to toggle navBar
+  const showNav = () =>{
+    setActive('navBar activeNavbar')
+  }
+
+  // Function to remove navBar
+  const removeNavbar = () =>{
+    setActive('navBar')
+  }
+
   return (
-    <section className='navbarSection'>
+    <section className='navBarSection'>
         <header className='header flex'>
            <div className='logoDiv'>
             <a href="#" className="logo flex">
@@ -14,7 +25,7 @@ const Navbar = () => {
             </a>
             </div> 
 
-            <div className="navBar">
+            <div className={active}>
               <ul className='navLists flex'>
                 <li className='navItem'>
                     <a href="#" className="navLink">Home</a>
@@ -43,15 +54,17 @@ const Navbar = () => {
                 </button>
               </ul>
 
-              <div className="closeNavbar">
+              <div onClick={removeNavbar}
+               className="closeNavbar">
                 <AiFillCloseCircle className="icon"/>
               </div>
 
-              <div className="toggleNavbar">
+            </div>
+
+            <div  onClick= {showNav}
+            className="toggleNavbar">
                 <TbGridDots className="icon"/>
               </div>
-
-            </div>
         </header>
     </section>
   );
